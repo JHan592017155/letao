890430b4f5b02bb68c0d520a9447465edab9045b
JHan592017155 <592017155@qq.com>
2018-11-01 16:28:26 +0800

    用户管理完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,11 +1,22 @@
 $(function(){
+    var currentPage=1;  //全局的页码
+    var pageSize=5;  //每页多少条
+
+    var currentId;  //当前修改的用户 id
+    var isDelete;  //修改的状态
+
+
+    render();
   // 一进入页面, 发送 ajax 请求, 获取用户列表, 通过 模板引擎渲染
+
+    function render(){
+           
     $.ajax({
         type:"get",
         url:"/user/queryUser",
         data:{
-            page:1,
-            pageSize:5
+            page:currentPage,
+            pageSize:pageSize
         },
         dataType:"json",
         success:function(info){
@@ -18,8 +29,73 @@ $(function(){
 
             //渲染到页面中
             $('tbody').html(htmlStr);
+
+
+            //配置分页插件
+            $('#paginator').bootstrapPaginator({
+                //配置版本号
+                bootstrapMajorVersion: 3,
+                //总页数
+                totalPages:Math.ceil(info.total/info.size),
+                //当前页
+                currentPage: info.page,
+                //页码点击事件
+                onPageClicked:function(a,b,c,page){
+                    currentPage=page;
+                    render()
+                }
+            });
+
         }
         
+    })
+
+
+    }
+
+
+  // 什么时候用事件委托?
+  // 1. 元素是动态生成的
+  // 2. 批量注册事件, 效率高 
+
+  // 点击启用禁用按钮, 显示模态框 (使用事件委托)
+    $('tbody').on("click",".btn",function(){
+        //显示模态框
+        $('#userModal').modal('show');
+
+        //获取父元素 td 中存储的 data-id
+        currentId=$(this).parent().data("id");
+
+        //获取启用还是禁用，根据按钮的类来判断
+        //禁用？ 0 ： 1；
+
+        isDelete=$(this).hasClass("btn-danger") ? 0 : 1 ;
+    });
+
+    //点击模态框确定按钮 进行修改用户
+
+    $('#submitBtn').click(function(){
+
+        $.ajax({
+            type:"POST",
+            url:"/user/updateUser",
+            data: {
+                id:currentId,
+                isDelete:isDelete
+            },
+            dataType:"json",
+            success:function(info){
+                console.log(info);
+
+                //关闭模态框
+                $('#userModal').modal("hide");
+
+                //重新渲染当前页
+                render();
+
+            }
+        })
+
     })
 
 
