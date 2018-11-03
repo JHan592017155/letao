07d77ddbd81d1f07846ec2b4a606149b07be62c7
JHan592017155 <592017155@qq.com>
2018-11-03 23:59:28 +0800

    乐淘3用户管理模板开始

--- public/back3/js/common.js
+++ public/back3/js/common.js
@@ -34,3 +34,46 @@ $(document).ajaxStop(function(){
     NProgress.done();
   }, 500 );
 });
+
+
+$(function(){
+  //1. 二级导航的切换
+  $('.lt_aside .nav .category').click(function(){
+    $(this).next().stop().slideToggle();
+  });
+
+  //2-左侧菜单的切换
+  $('.lt_topbar .icon_menu').click(function(){
+    // 切换侧边栏, hidemenu 表示隐藏侧边栏的状态
+    $('.lt_aside').toggleClass("hidemenu");
+    $('.lt_main').toggleClass("hidemenu");
+    $('.lt_topbar').toggleClass("hidemenu");
+  })
+
+  //3-退出功能的实现
+  $('.lt_topbar .icon_logout').click(function(){
+    $('#logoutModal').modal("show");
+  });
+
+   // 退出功能
+  // 退出登陆的方式:
+  // (1) 用户端(浏览器端), 用户自己清除浏览器缓存  (清空了 cookie),
+  //     本质上将会话标识 sessionId 也清除了
+  // (2) 前端通过发送ajax退出请求, 让后台销毁当前用户的登录状态
+
+  $('#logoutBtn').click(function(){
+    $.ajax({
+      type:"get",
+      url:"/employee/employeeLogout",
+      dataType:"json",
+      success:function(info){
+        console.log(info);
+        if (info.success) {
+          location.href="login.html";
+        }
+      }
+    })
+  })
+
+
+})
